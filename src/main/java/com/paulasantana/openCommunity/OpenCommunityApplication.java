2f8aa4582fdca0aa0da6ff3ff99cedb530c067ad
Paula Santana <psanrosa13@gmail.com>
2020-07-15 20:47:22 -0300

    setup do projeto

---
+++ src/main/java/com/paulasantana/openCommunity/OpenCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.paulasantana.openCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenCommunityApplication.class, args);
+	}
+
+}
